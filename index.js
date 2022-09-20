8daa8d21b29c93760e4806ce470fe85ed70053dc
Vincent Toon <vincenttoon22@gmail.com>
2022-09-19 22:29:05 -0500

    requires added

---
+++ index.js
@@ -0,0 +1,12 @@
+// require job variables
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+// require inquirer
+const inquirer = require('inquirer');
+// require fs
+const fs = require('fs');
+
+
+
