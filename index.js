cf3a07d22495c862c0d7b09ea443cef9f10fdcbc
Vincent Toon <vincenttoon22@gmail.com>
2022-09-19 22:53:07 -0500

    manager function working, empty array was the right idea

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const inquirer = require('inquirer');
 // require fs
 const fs = require('fs');
 
+// Empty team array for results
+const team = [];
+
 const init = () => {
     console.log("It's time to build your team!");
     inquirer
@@ -15,9 +18,70 @@ const init = () => {
             {
                 type: 'input',
                 name: 'managerName',
-                message: 'What is your managers name?'
+                message: "What is your manager's name?",
+                // this needs rubular
+                validate: (response) => {
+                    if (response) {
+                        return true;
+                    } else {
+                        console.log("Please insert your manager's name!")
+                        return false
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'managerId',
+                message: "What is your manager's id number?",
+                // this needs rubular
+                validate: (response) => {
+                    if (response) {
+                        return true;
+                    } else {
+                        console.log("Please insert your managers id number!")
+                        return false
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'managerEmail',
+                message: "What is your manager's email address?",
+                // this needs rubular
+                validate: (response) => {
+                    if (response) {
+                        return true;
+                    } else {
+                        console.log("Please insert your managers id number!")
+                        return false
+                    }
+                }
+            },
+            {
+                type: 'input',
+                name: 'managerOffice',
+                message: "What is your manager's office phone number?",
+                 // this needs rubular
+                validate: (response) => {
+                    if (response) {
+                        return true;
+                    } else {
+                        console.log("Please insert your managers id number!")
+                        return false
+                    }
+                }
             }
         ])
+        .then((responses) => {
+            const manager = new Manager(
+                responses.managerName,
+                responses.managerId,
+                responses.managerEmail,
+                responses.managerOffice
+            );
+            team.push(manager);
+            console.log(team);
+        })
 }
 
 // function to start application
