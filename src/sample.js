c3f51ae65f2354ab822c21827c58f9ef013a14bd
Vincent Toon <vincenttoon22@gmail.com>
2022-09-20 20:45:48 -0500

    literal html added with data parameters

--- src/sample.js
+++ src/sample.js
@@ -1,32 +1,98 @@
-const generateManger = (workingTeam) => {
+const generateTeam = (team) => {
+  const generateManger = (manager) => {
     // !!! would create separate cards for Manager !!!
     return `
-    
-    `
-}
+        <div class="card-body col-4">
+            <div class="border border-warning">
+                <div class="card-header">
+                    <h5 class="card-title">${manager.getName()}</h5>
+                    <h6 class="card-subtitle"><i class="fab fa-black-tie"></i>  Manager</h6>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID Number: ${manager.getId()}</li>
+                    <li class="list-group-item">Email:<a href="${manager.getEmail()}"></a></li>
+                    <li class="list-group-item">Office Number: ${manager.getOfficeNumber()}</li>
+                </ul>
+            </div>
+        </div>
+        `;
+  };
 
-const generateEngineer = (workingTeam) => {
+  const generateEngineer = (engineer) => {
     // !!! would create separate cards for Engineer !!!
     return `
-    
-    `
-}
+        <div class="card-body col-4">
+            <div class="border border-warning">
+                <div class="card-header">
+                    <h5 class="card-title">${engineer.getName()}</h5>
+                    <h6 class="card-subtitle"><i class="fas fa-cogs"></i>  ${engineer.getRole()}</h6>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">ID Number: ${engineer.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${engineer.getEmail()}">${engineer.getEmail()}</a></li>
+                    <li class="list-group-item">Github: <a href="https://github.com/${engineer.getGithub()}">${engineer.getGithub()}</a></li>
+                </ul>
+            </div>
+        </div>
+        
+        `;
+  };
 
-const generateIntern = (workingTeam) => {
+  const generateIntern = (intern) => {
     // !!! would create separate cards for Intern !!!
     return `
-    
-    `
-}
+      <div class="card-body col-4">
+          <div class="border border-warning">
+              <div class="card-header">
+                  <h5 class="card-title">${intern.getName()}</h5>
+                  <h6 class="card-subtitle"><i class="fas fa-graduation-cap"></i>  ${intern.getName()}</h6>
+              </div>
+              <ul class="list-group list-group-flush">
+                  <li class="list-group-item">ID Number: ${intern.getId()}</li>
+                  <li class="list-group-item">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></li>
+                  <li class="list-group-item">School: ${intern.getSchool()}</li>
+              </ul>
+                        </div>
+    </div>
+      `;
+  };
+};
 
 // !!! Need to join workingTeam into one collective? Probably as workingTeam? !!!
 
 // !!! Would generate the final HTML with the above parameters/cards included
-const generateHtml = (workingTeam) => {
-    return `
+function generateHtml() {
+  return `
+  <!DOCTYPE html>
+    <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet" />
+    <title>Team Builder</title>
+  </head>
+
+  <body>
+    <div class="row">
+        <div class="bg-info col-12 jumbotron container text-center">
+            <h1 style="text-decoration:underline;">Your Professional Team:</h1>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="d-flex justify-content-between" style="width: 80%;">
+
+            ${generateTeam(team)}
+
+        </div>
+    </div>
+  </body>
     
-    `
-}
+    `;
+};
 
 // !!! export to use generateHtml for the writePage call? !!!
-module.exports = {generateHtml};
+module.exports = { generateHtml };
